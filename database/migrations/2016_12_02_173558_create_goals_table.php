a0ce349583a4c56f5e86cf7e178e729fbd50c99c
Eric Ehmann <eaehmann@gmail.com>
2016-12-04 17:50:48 -0500

    Created controllers

---
+++ database/migrations/2016_12_02_173558_create_goals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    	Schema::create('goals', function (Blueprint $table) {
+    	
+        $table->increments('id');
+        $table->timestamps();
+        $table->enum('type', ['running distance', 'running pace', 'workout', ]);
+        $table->string('description'); // Any string based data
+        $table->float('quantifier', 7, 2);  //Numerical expression of workout
+        $table->float('starting_point', 7,2); // What can be done on day one
+        $table->boolean('completed');
+        $table->date('completed_on')->nullable();
+    	});
+    }
+	
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('goals'); 
+    }
+}
---
+++ database/migrations/2016_12_02_173611_create_workouts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+          Schema::create('workouts', function (Blueprint $table) {
+
+        $table->increments('id');
+        $table->timestamps();
+        $table->enum('type', ['running distance', 'running pace', 'workout', ]);
+        $table->string('workdescription');      // what was the exercise
+        $table->float('workquantifier', 7, 2); // number of miles ran, pace, load etc... 
+       
+
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::drop('workouts');
+    }
+}
